a7925a246687f60a11b98a977d6715028a8be160
Genevieve DuPuy <genevieve.e.dupuy@gmail.com>
2023-04-27 22:08:08 -0700

    finished lab JS

--- js/app.js
+++ js/app.js
@@ -1,55 +1,164 @@
 'use strict';
 
-let answerCats = prompt(
-  'Let\'s play Get to Know Genevieve! Here\'s an easy warm up question: Do I like cats: yes or no?');
-while (answerCats === '' || answerCats === null) {
-  answerCats = prompt('Aw, come on! Let\'s play! Do I like cats: yes or no?');
-}
+let answerCats = '';
 
-answerCats = answerCats.trim().toLowerCase();
-console.log('like cats', answerCats);
+while(answerCats.trim().toLowerCase() !== 'yes' && answerCats.trim().toLowerCase() !== 'y' && answerCats.trim().toLowerCase() !== 'no' && answerCats.trim().toLowerCase() !== 'n')
+{
+  answerCats = prompt('Let\'s play Get to Know Genevieve! Here\'s an easy warm up question: Do I like cats: yes or no?');
+  while (answerCats === '' || answerCats === null)
+  {
+    answerCats = prompt('Aw, come on! Do I like cats: yes or no?');
+  }
 
-switch (answerCats) {
-case 'yes':
-// console.log('Yay! It\'s like you know me already!');
-  alert('Yay! It\'s like you know me already!');
-  break;
-case 'no':
-// console.log('Oof! We\'re off to a bad start here.');
-  alert('Oof! We\'re off to a bad start here.');
-  break;
-default:
+  answerCats = answerCats.trim().toLowerCase();
+  console.log('like cats', answerCats);
+
+  switch (true)
+  {
+  case (answerCats === 'yes' || answerCats === 'y'):
+  // console.log('Yay! It\'s like you know me already!');
+    alert('Yay! It\'s like you know me already!');
+    break;
+  case (answerCats === 'no' || answerCats === 'n'):
+  // console.log('Oof! We\'re off to a bad start here.');
+    alert('Oof! We\'re off to a bad start here.');
+    break;
+  default:
   // console.log('Only yes or no answers, purrlease.');
-  answerCats = prompt('Only yes or no answers, purrlease. Do I like cats?');
-  break;
-  // how can i loop this particular 'purrlease' prompt under default so 1) they can't cancel out, and 2) it goes back to my yes and no case alerts above?
+    alert('Only yes or no answers, purrlease.');
+    break;
+  }
 }
 
-let answerCook = prompt('Am I a good cook: yes or no?');
-while (answerCook === '' || answerCook === null) {
-  answerCook = prompt('Only yes or no answers, if you don\'t mind.');
-}
 
-answerCook = answerCook.trim().toLowerCase();
-console.log('good cook', answerCook);
+let answerCook = '';
 
-switch (answerCook) {
-case 'yes':
+while(answerCook.trim().toLowerCase() !== 'yes' && answerCook.trim().toLowerCase() !== 'y' && answerCook.trim().toLowerCase() !== 'no' && answerCook.trim().toLowerCase() !== 'n')
+{
+  answerCook = prompt('Am I a good cook: yes or no?');
+  while (answerCook === '' || answerCook === null)
+  {
+    answerCook = prompt('Only yes or no answers, if you don\'t mind.');
+  }
+
+  answerCook = answerCook.trim().toLowerCase();
+  console.log('good cook', answerCook);
+
+  switch (true)
+  {
+  case (answerCook === 'yes' || answerCook === 'y'):
   // console.log('You have such faith in me, shucks!');
-  alert('You have such faith in me, shucks!');
-  break;
-case 'no':
+    alert('You have such faith in me, shucks!');
+    break;
+  case (answerCook === 'no' || answerCook === 'n'):
   // console.log('Unfortunately, you\'re not wrong.');
-  alert('Unfortunately, you\'re not wrong.');
-  break;
-default:
+    alert('Unfortunately, you\'re not wrong.');
+    break;
+  default:
   // console.log('Only yes or no answers, friend: Am I a good cook? I can take it.');
-  answerCats = prompt(
-    'Only yes or no answers, friend: Am I a good cook? I can take it.');
-  break;
-  // Loop???
+    alert('Only yes or no answers, friend. I can take it.');
+    break;
+  }
 }
 
-// 3. Can I roller skate: yes or no?
-// 4. Do I sunburn easily: yes or no?
-// 5. And for all the marbles: Can I pet a cat while roller skating and applying sunscreen, all while remembering to take my dinner out of the oven before it burns: yes or no??
+
+let answerSkate = '';
+
+while(answerSkate.trim().toLowerCase() !== 'yes' && answerSkate.trim().toLowerCase() !== 'y' && answerSkate.trim().toLowerCase() !== 'no' && answerSkate.trim().toLowerCase() !== 'n')
+{
+  answerSkate = prompt('Can I roller skate: yes or no?');
+  while (answerSkate === '' || answerSkate === null)
+  {
+    answerSkate = prompt('Stick to yes or no answers, please.');
+  }
+
+  answerSkate = answerSkate.trim().toLowerCase();
+  console.log('can skate', answerSkate);
+
+  switch (true)
+  {
+  case (answerSkate === 'yes' || answerSkate === 'y'):
+  // console.log('I could when I was 12! As an adult it\'s tbd...');
+    alert('I could when I was 12! As an adult it\'s tbd...');
+    break;
+  case (answerSkate === 'no' || answerSkate === 'n'):
+  // console.log('We\'ll find out when my new skates arrive!');
+    alert('We\'ll find out when my new skates get here!');
+    break;
+  default:
+  // console.log('A simple yes or no will suffice, thanks.');
+    alert('A simple yes or no will suffice.');
+    break;
+  }
+}
+
+
+let answerSunburn = '';
+
+while(answerSunburn.trim().toLowerCase() !== 'yes' && answerSunburn.trim().toLowerCase() !== 'y' && answerSunburn.trim().toLowerCase() !== 'no' && answerSunburn.trim().toLowerCase() !== 'n')
+{
+  answerSunburn = prompt('Do I sunburn easily: yes or no?');
+  while (answerSunburn === '' || answerSunburn === null)
+  {
+    answerSunburn = prompt('A yes or no will do just fine.');
+  }
+
+  answerSunburn = answerSunburn.trim().toLowerCase();
+  console.log('sunburn easily', answerSunburn);
+
+  switch (true)
+  {
+  case (answerSunburn === 'yes' || answerSunburn === 'y'):
+  // console.log('Did my million and one moles tip you off?');
+    alert('Did my million and one moles tip you off?');
+    break;
+  case (answerSunburn === 'no' || answerSunburn === 'n'):
+  // console.log('I don\'t spontaneously combust like a vampire, but it\'s close.');
+    alert('I don\'t spontaneously combust like a vampire, but it\'s close.');
+    break;
+  default:
+  // console.log('Yes or no answers only.');
+    alert('Yes or no answers only.');
+    break;
+  }
+}
+
+
+let answerMarbles = '';
+
+while(answerMarbles.trim().toLowerCase() !== 'yes' && answerMarbles.trim().toLowerCase() !== 'y' && answerMarbles.trim().toLowerCase() !== 'no' && answerMarbles.trim().toLowerCase() !== 'n')
+{
+  answerMarbles = prompt('For all the marbles: Can I pet a cat while applying sunscreen to go out roller skating and simultaneously remember to take my dinner out of the oven before I leave and it burns: yes or no?!?');
+  while (answerMarbles === '' || answerMarbles === null)
+  {
+    answerMarbles = prompt('This is the big one, folks. Only yes or no will do.');
+  }
+
+  answerMarbles = answerMarbles.trim().toLowerCase();
+  console.log('good cook', answerMarbles);
+
+  switch (true)
+  {
+  case (answerMarbles === 'yes' || answerMarbles === 'y'):
+  // console.log('That\'s it! You\'re hired as my new inspirational coach!!!');
+    alert('That\'s it! You\'re hired as my new inspirational coach!!!');
+    break;
+  case (answerMarbles === 'no' || answerMarbles === 'n'):
+  // console.log('I mean, seriously! WHO could do all o\' that?!');
+    alert('I mean, seriously! WHO could do all o\' that?!');
+    break;
+  default:
+  // console.log('Rules are rules. It\'s either a yes, or it\'s a no. No pressure.');
+    alert('Rules are rules. It\'s either a yes, or it\'s a no. No pressure.');
+    break;
+  }
+}
+
+let userName = prompt ('All this talk about me, but what about you? What\'s your name?');
+while (userName === '' || userName === null)
+{
+  userName = prompt ('Don\'t be shy now!');
+}
+userName = userName.trim().toLowerCase();
+console.log('username is', userName);
+alert('Hello, ' + userName + ' it\'s been so much fun playing guessing games with you! I hope you enjoy my website!');
