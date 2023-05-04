90dbc640728639a5e24fb0085730d56b27707589
Genevieve DuPuy <genevieve.e.dupuy@gmail.com>
2023-05-03 18:29:32 -0700

    deleted extra spacing in js

--- js/app.js
+++ js/app.js
@@ -10,16 +10,13 @@ alert('Hello, ' + userName + '! I hope you enjoy my website.');
 let correctAnswers = 0;
 
 let answerCats = '';
-
 while (answerCats !== 'yes' && answerCats !== 'y' && answerCats !== 'no' && answerCats !== 'n') {
   answerCats = prompt('Let\'s play Get to Know Genevieve. Here\'s an easy warm up question: Do I like cats: yes or no?');
   while (answerCats === '' || answerCats === null) {
     answerCats = prompt('Aw, come on! Do I like cats: yes or no?');
   }
-
   answerCats = answerCats.trim().toLowerCase();
   // console.log('like cats', answerCats);
-
   switch (true) {
   case (answerCats === 'yes' || answerCats === 'y'):
   // console.log('Yay! It\'s like you know me already!');
@@ -39,16 +36,13 @@ while (answerCats !== 'yes' && answerCats !== 'y' && answerCats !== 'no' && answ
 
 
 let answerCook = '';
-
 while (answerCook !== 'yes' && answerCook !== 'y' && answerCook !== 'no' && answerCook !== 'n') {
   answerCook = prompt('Am I a good cook: yes or no?');
   while (answerCook === '' || answerCook === null) {
     answerCook = prompt('Only yes or no answers, if you don\'t mind.');
   }
-
   answerCook = answerCook.trim().toLowerCase();
   // console.log('good cook', answerCook);
-
   switch (true) {
   case (answerCook === 'yes' || answerCook === 'y'):
   // console.log('You have such faith in me, shucks!');
@@ -68,16 +62,13 @@ while (answerCook !== 'yes' && answerCook !== 'y' && answerCook !== 'no' && answ
 
 
 let answerSkate = '';
-
 while (answerSkate !== 'yes' && answerSkate !== 'y' && answerSkate !== 'no' && answerSkate !== 'n') {
   answerSkate = prompt('Can I roller skate: yes or no?');
   while (answerSkate === '' || answerSkate === null) {
     answerSkate = prompt('Stick to yes or no answers, please.');
   }
-
   answerSkate = answerSkate.trim().toLowerCase();
   // console.log('can skate', answerSkate);
-
   switch (true) {
   case (answerSkate === 'yes' || answerSkate === 'y'):
   // console.log('I could when I was 12! As an adult it\'s tbd...');
@@ -97,16 +88,13 @@ while (answerSkate !== 'yes' && answerSkate !== 'y' && answerSkate !== 'no' && a
 
 
 let answerSunburn = '';
-
 while (answerSunburn !== 'yes' && answerSunburn !== 'y' && answerSunburn !== 'no' && answerSunburn !== 'n') {
   answerSunburn = prompt('Do I sunburn easily: yes or no?');
   while (answerSunburn === '' || answerSunburn === null) {
     answerSunburn = prompt('A yes or no will do just fine.');
   }
-
   answerSunburn = answerSunburn.trim().toLowerCase();
   // console.log('sunburn easily', answerSunburn);
-
   switch (true) {
   case (answerSunburn === 'yes' || answerSunburn === 'y'):
   // console.log('Did my million and one moles tip you off?');
@@ -126,16 +114,13 @@ while (answerSunburn !== 'yes' && answerSunburn !== 'y' && answerSunburn !== 'no
 
 
 let answerMarbles = '';
-
 while (answerMarbles !== 'yes' && answerMarbles !== 'y' && answerMarbles !== 'no' && answerMarbles !== 'n') {
   answerMarbles = prompt('For all the marbles: Can I pet a cat while applying sunscreen to go out roller skating and simultaneously remember to take my dinner out of the oven before I leave and it burns: yes or no?!?');
   while (answerMarbles === '' || answerMarbles === null) {
     answerMarbles = prompt('This is the big one, folks. Only yes or no will do.');
   }
-
   answerMarbles = answerMarbles.trim().toLowerCase();
   // console.log('good cook', answerMarbles);
-
   switch (true) {
   case (answerMarbles === 'yes' || answerMarbles === 'y'):
   // console.log('That\'s it! You\'re hired as my new inspirational coach!!!');
@@ -153,15 +138,13 @@ while (answerMarbles !== 'yes' && answerMarbles !== 'y' && answerMarbles !== 'no
   }
 }
 
+
 let number = 2;
 let pickNumInt;
-
 for(let i = 0; i < 4; i++) {
   let pickNum = prompt('Pick a number between 1 and 5.');
   // console.log('pickNum', pickNum);
-
   pickNumInt = parseInt(pickNum);
-
   if(isNaN(pickNumInt) || pickNumInt === '' || pickNumInt === null){
     alert('Oopsie! That\'s not a number. Try again.');
   }
@@ -181,6 +164,7 @@ if(pickNumInt !== number){
   alert('Shoot, you ran out of guesses. The number was 2.');
 }
 
+
 let catNames = ['Fox', 'Sophie', 'Oliver', 'Albus','Bug'];
 let hasGuessedRight = false;
 for(let i = 0; i < 6; i++){
@@ -199,21 +183,17 @@ for(let i = 0; i < 6; i++){
   } else {
     alert('Nope. Try again!');
   }
-
 }
 if(!hasGuessedRight){
   alert('Bummer. Good guesses, though.');
 }
-
 let catNamesString = '';
 for(let i = 0; i < catNames.length - 1; i++){
   catNamesString = catNamesString + catNames[i] + ', ';
 }
 catNamesString = catNamesString + 'and ' + catNames[catNames.length - 1] + '.';
-
 alert('A+ for effort. My cat\'s names are ' + catNamesString);
 
-alert('Congrats! You got ' + correctAnswers + ' correct!');
 
+alert('Congrats! You got ' + correctAnswers + ' correct out of 7!');
 alert('It was so fun playing this game with you today, ' + userName + '! I hope you had a good time.');
-
